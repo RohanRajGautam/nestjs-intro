e90af52a05850ea1870e29b73b710783aac0aef7
Rohan Raj Gautam <rohangautamdev@gmail.com>
2021-06-19 13:06:53 +0545

    added get and post route for products

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): { name: string } {
+    return {
+      name: 'rohan',
+    };
+    // return this.appService.getHello();
   }
 }
